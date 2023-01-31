75d747025ad2f401387d9b00b89096ae9c70936e
vinaykumar guddeti <vickyvickychinna143@gmail.com>
2023-01-31 21:41:21 +0530

    second commit

--- MicroProject2/Microproject2/Program.cs
+++ MicroProject2/Microproject2/Program.cs
@@ -10,6 +10,7 @@ namespace Microproject2
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+            //Testing
             ApplicationConfiguration.Initialize();
             Application.Run(new Create_User());
         }
